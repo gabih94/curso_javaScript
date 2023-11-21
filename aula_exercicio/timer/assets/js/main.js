af0d4598fe78cdf6d81d9c9825cb140600856d8f
Gabriela Maria Romualdo <88865315+gabih94@users.noreply.github.com>
2023-11-21 17:03:42 -0300

    add desafio

---
+++ aula_exercicio/timer/assets/js/main.js
@@ -0,0 +1,31 @@
+const relogio = document.querySelector('.hour');
+const iniciar = document.querySelector('.btn-iniciar');
+const pausar = document.querySelector('.btn-pausar');
+const resetar = document.querySelector('.btn-resetar');
+
+// function hour() {
+//     let date = new Date()
+
+//     return date.toLocaleTimeString('pt-BR', {
+//         hour: '2-digit',
+//         minute: '2-digit',
+//         second: '2-digit',
+//         hour12: false
+//     })
+// }
+// const inter = setInterval(function() {
+//     console.log(hour())
+// }, 1000)
+
+iniciar.addEventListener('click', function(event) {
+    relogio.innerHTML()
+})
+
+pausar.addEventListener('click', function(event) {
+    alert('Click')
+})
+
+resetar.addEventListener('click', function(event) {
+    alert('Click')
+})
+
